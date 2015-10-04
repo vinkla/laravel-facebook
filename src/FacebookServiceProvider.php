7549f875a93170cedb6332ff90f1fee6b72b687d
Vincent Klaiber <vincentklaiber@gmail.com>
2015-10-04 14:56:40 +0200

    Improved Lumen support

--- src/FacebookServiceProvider.php
+++ src/FacebookServiceProvider.php
@@ -29,20 +29,24 @@ class FacebookServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig();
+        $this->setupConfig($this->app);
     }
 
     /**
      * Setup the config.
      *
+     * @param \Illuminate\Contracts\Foundation\Application $app
+     *
      * @return void
      */
-    protected function setupConfig()
+    protected function setupConfig(Application $app)
     {
         $source = realpath(__DIR__.'/../config/facebook.php');
 
         if (class_exists('Illuminate\Foundation\Application', false)) {
             $this->publishes([$source => config_path('facebook.php')]);
+        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+            $app->configure('facebook');
         }
 
         $this->mergeConfigFrom($source, 'facebook');
