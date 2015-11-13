c83184741c1b9cb24a5ea5d321405c14ecb60bd0
Vincent Klaiber <vincentklaiber@gmail.com>
2015-11-13 09:07:33 +0100

    Improved environment specific code

--- src/FacebookServiceProvider.php
+++ src/FacebookServiceProvider.php
@@ -13,7 +13,9 @@ namespace Schimpanz\Facebook;
 
 use Facebook\Facebook;
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 
 /**
  * This is the Facebook service provider class.
@@ -43,9 +45,9 @@ class FacebookServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/facebook.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('facebook.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('facebook');
         }
 
