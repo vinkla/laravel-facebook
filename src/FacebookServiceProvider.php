5b415232673ce3d2684af1d4d04a331129bad084
Vincent Klaiber <vincentklaiber@gmail.com>
2016-01-24 14:49:08 +0100

    Tweak config booting

--- src/FacebookServiceProvider.php
+++ src/FacebookServiceProvider.php
@@ -31,24 +31,22 @@ class FacebookServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setupConfig($this->app);
+        $this->setupConfig();
     }
 
     /**
      * Setup the config.
      *
-     * @param \Illuminate\Contracts\Container\Container $app
-     *
      * @return void
      */
-    protected function setupConfig(Application $app)
+    protected function setupConfig()
     {
         $source = realpath(__DIR__.'/../config/facebook.php');
 
-        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
             $this->publishes([$source => config_path('facebook.php')]);
-        } elseif ($app instanceof LumenApplication) {
-            $app->configure('facebook');
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('facebook');
         }
 
         $this->mergeConfigFrom($source, 'facebook');
